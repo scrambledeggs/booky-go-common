572c261069c1dab4bd544049459cf7137eb32082
Cecille Sevilla <cecillegsevilla01@gmail.com>
2019-10-02 17:22:00 +0800

    remove null check for int, float and bool

--- custommarshal/custommarshal_test.go
+++ custommarshal/custommarshal_test.go
@@ -11,17 +11,21 @@ type Brand struct {
 	Description *string `json:"description,omitempty"`
 	Kind        string  `json:"kind,omitempty"`
 	Status      string  `json:"brand_status,omitempty"`
+	Limit       *int    `json:"offer_limit,omitempty"`
 }
 
 func TestMap(t *testing.T) {
 	description := ""
+	zero := 0
 	brand := Brand{
 		Name:        "Renamed Brand 1",
 		Status:      "inactive",
 		Description: &description,
+		Limit:       &zero,
 	}
 
 	res, err := Map(brand)
 	assert.Equal(t, true, *res["description"].NULL)
+	assert.Equal(t, "0", *res["offer_limit"].N)
 	assert.Nil(t, err)
 }
