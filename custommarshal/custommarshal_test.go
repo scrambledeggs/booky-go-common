b6702f0a47c42a45bcf6e3afc9723220ef231320
Cecille Sevilla <cecillegsevilla01@gmail.com>
2019-10-02 16:02:49 +0800

    update test

--- custommarshal/custommarshal_test.go
+++ custommarshal/custommarshal_test.go
@@ -3,24 +3,25 @@ package custommarshal
 import (
 	"testing"
 
-	"github.com/scrambledeggs/booky-brand-csmr/internal/entities"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCustomMarshalMap(t *testing.T) {
+type Brand struct {
+	Name        string  `json:"brand_name,omitempty"`
+	Description *string `json:"description,omitempty"`
+	Kind        string  `json:"kind,omitempty"`
+	Status      string  `json:"brand_status,omitempty"`
+}
+
+func TestMap(t *testing.T) {
 	description := ""
-	brand := entities.CombinedData{
-		BrandData: entities.BrandData{
-			BrandKey: entities.BrandKey{BrandID: 1},
-			Brand: entities.Brand{
-				Name:        "Renamed Brand 1",
-				Status:      "inactive",
-				Description: &description,
-			},
-		},
+	brand := Brand{
+		Name:        "Renamed Brand 1",
+		Status:      "inactive",
+		Description: &description,
 	}
 
-	res, err := CustomMarshalMap(brand)
+	res, err := Map(brand)
 	assert.Equal(t, true, *res["description"].NULL)
 	assert.Nil(t, err)
 }
