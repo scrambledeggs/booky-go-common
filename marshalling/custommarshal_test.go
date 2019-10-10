428dcbcb78891786f536dbe5c7c957631c58369a
Cecille Sevilla <cecillegsevilla01@gmail.com>
2019-10-10 11:50:50 +0800

    check maps recursively for empty values

--- marshalling/custommarshal_test.go
+++ marshalling/custommarshal_test.go
@@ -7,32 +7,39 @@ import (
 )
 
 type Offer struct {
-	Title string `json:"title,omitempty"`
+	Title *string `json:"title,omitempty"`
+}
+
+type Listing struct {
+	Name *string `json:"listing_name,omitempty"`
 }
 
 type Brand struct {
-	Name        string  `json:"brand_name,omitempty"`
-	Description *string `json:"description,omitempty"`
-	Kind        string  `json:"kind,omitempty"`
-	Status      string  `json:"brand_status,omitempty"`
-	Limit       *int    `json:"offer_limit,omitempty"`
-	Offer       *Offer  `json:"offer"`
+	Name        string   `json:"brand_name,omitempty"`
+	Description *string  `json:"description,omitempty"`
+	Kind        string   `json:"kind,omitempty"`
+	Status      string   `json:"brand_status,omitempty"`
+	Limit       *int     `json:"offer_limit,omitempty"`
+	Offer       *Offer   `json:"offer"`
+	Listing     *Listing `json:"listing"`
 }
 
 func TestCustomMarshalMap(t *testing.T) {
 	description := ""
 	zero := 0
+	offer := Offer{&description}
 	brand := Brand{
 		Name:        "Renamed Brand 1",
 		Status:      "inactive",
 		Description: &description,
 		Limit:       &zero,
-		Offer:       &Offer{},
+		Offer:       &offer,
 	}
 
 	res, err := CustomMarshalMap(brand)
 	assert.Equal(t, true, *res["description"].NULL)
 	assert.Equal(t, "0", *res["offer_limit"].N)
-	assert.Equal(t, true, *res["offer"].NULL)
+	assert.Equal(t, true, *res["listing"].NULL)
+	assert.Equal(t, true, *res["offer"].M["title"].NULL)
 	assert.Nil(t, err)
 }
