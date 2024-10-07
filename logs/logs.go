a23242b7fd3d4b0277cbbe92ec3c0f2600e186ca
Alec Guintu <aeguintu@gmail.com>
2024-10-07 19:31:00 +0800

    Add Print and prints plainly. Optionalized data as well.

--- logs/logs.go
+++ logs/logs.go
@@ -9,73 +9,88 @@ import (
 	"os"
 )
 
-const (
-	DEBUG = "Debug"
-	INFO  = "Info"
-	TRACE = "Trace"
-	WARN  = "Warn"
-	ERROR = "Error"
-	FATAL = "Fatal"
+type Level string
 
-	PRODUCTION_ENV = "production"
+const (
+	DEBUG Level = "Debug"
+	PRINT Level = "Print"
+	INFO  Level = "Info"
+	TRACE Level = "Trace"
+	WARN  Level = "Warn"
+	ERROR Level = "Error"
+	FATAL Level = "Fatal"
 )
 
+const PRODUCTION_ENV = "production"
+
 type logEntry struct {
-	Level    string   `json:"level"`
-	Env      []string `json:"env"`
-	Request  any      `json:"request"`
-	Function string   `json:"function"`
-	AppEnv   string   `json:"app_env"`
+	Level    Level    `json:"level"`
+	Env      []string `json:"env,omitempty"`
+	Request  any      `json:"request,omitempty"`
+	Function string   `json:"function,omitempty"`
+	AppEnv   string   `json:"app_env,omitempty"`
 	Note     string   `json:"note"`
-	Data     any      `json:"data"`
+	Data     any      `json:"data,omitempty"`
 }
 
 var Request any
 
 // For development debugging. Will not log on Production.
 // Requires `APP_ENV` equal `production`
-func Debug(note string, data any) {
+func Debug(note string, data ...any) {
 	if os.Getenv("APP_ENV") == PRODUCTION_ENV {
 		return
 	}
 
-	logIt(DEBUG, note, data)
+	logIt(DEBUG, note, data...)
+}
+
+// For development logging only. Will not log on Production.
+// Requires `APP_ENV` equal `production`
+func Print(note string, data ...any) {
+	logIt(PRINT, note, data...)
 }
 
 // Log used in event tracing like User clicked pay (CT like)
-func Trace(note string, data any) {
-	logIt(TRACE, note, data)
+func Trace(note string, data ...any) {
+	logIt(TRACE, note, data...)
 }
 
 // Basic level logging
-func Info(note string, data any) {
-	logIt(INFO, note, data)
+func Info(note string, data ...any) {
+	logIt(INFO, note, data...)
 }
 
 // Level for expected errors like ID not found
-func Warn(note string, data any) {
-	logIt(WARN, note, data)
+func Warn(note string, data ...any) {
+	logIt(WARN, note, data...)
 }
 
 // Level for "expected" crashes like can't connect to database
-func Error(note string, data any) {
-	logIt(ERROR, note, data)
+func Error(note string, data ...any) {
+	logIt(ERROR, note, data...)
 }
 
 // Level for system crashes
-func Fatal(note string, data any) {
-	logIt(FATAL, note, data)
+func Fatal(note string, data ...any) {
+	logIt(FATAL, note, data...)
 }
 
-func logIt(level string, note string, data any) {
+func logIt(level Level, note string, data ...any) {
 	le := logEntry{
-		Level:    level,
-		Env:      os.Environ(),
-		Request:  Request,
-		Function: os.Getenv("AWS_LAMBDA_FUNCTION_NAME"),
-		AppEnv:   os.Getenv("APP_ENV"),
-		Note:     note,
-		Data:     data,
+		Level: level,
+		Note:  note,
+	}
+
+	if len(data) > 0 {
+		le.Data = data[0]
+	}
+
+	if level != PRINT {
+		le.Env = os.Environ()
+		le.Request = Request
+		le.Function = os.Getenv("AWS_LAMBDA_FUNCTION_NAME")
+		le.AppEnv = os.Getenv("APP_ENV")
 	}
 
 	l, err := jsonMarshal(le)
@@ -87,7 +102,7 @@ func logIt(level string, note string, data any) {
 }
 
 func jsonMarshal(le logEntry) ([]byte, error) {
-	if le.Level == DEBUG {
+	if le.Level == DEBUG || le.Level == PRINT {
 		return json.MarshalIndent(le, "", "  ")
 	}
 
