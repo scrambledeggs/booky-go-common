3eab609854225c2f726d1963c5ffa1d4a21d4d1a
Alec Guintu <aeguintu@gmail.com>
2025-05-08 02:50:05 -0400

    Remove REGION being explicitly definied

--- idempotency/dynamo.go
+++ idempotency/dynamo.go
@@ -2,7 +2,6 @@ package idempotency
 
 import (
 	"context"
-	"os"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
@@ -14,10 +13,8 @@ import (
 // Parameters:
 //   - `dbUrl` mainly used for local debugging when passed with a local DynamoDB URL(i.e. "http://localhost:8000")
 func NewDynamoDBClient(ctx context.Context, dbUrl *string) *dynamodb.Client {
-	region := os.Getenv("AMZ_REGION")
-
-	// Load default configuration with region if specified
-	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	// Load default configuration without explicitly specifying region
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		panic(err.Error())
 	}
